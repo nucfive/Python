3905fd7eb9ea41e5643e635a7f86c6a22fc3617b
nucfive <18734914520@163.com>
2018-11-05 17:49:12 +0800

    基本使用

---
+++ ex2.py
@@ -0,0 +1,20 @@
+#numbers 整数，小数
+#string
+a = 5
+b = 10
+print(10 + 5)
+print(a + b)
+
+name = "wrf"
+print(name)
+#与数组类似
+print(name[0])
+#len求字符串的长度
+print(len(name))
+print(name[len(name) - 1])
+#打印中间的几个字符串
+print(name[0:2])
+#打印几次
+print(name * 2)
+
+
---
+++ ex3_list.py
@@ -0,0 +1,10 @@
+#list 数据类型
+my_list = ["ni hao", 2018, "你号", 2000]
+len(my_list)
+print(my_list[0])
+my_list.append("4000")
+print(my_list)
+my_list.remove("ni hao")
+print(my_list)
+print(my_list.count(2018))
+#help(my_list)
