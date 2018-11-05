322e14f044393e46d6a0585caec783ccd9b71ec7
nucfive <18734914520@163.com>
2018-11-05 18:08:36 +0800

    基本使用

--- ex4_tuple.py
+++ ex4_tuple.py
@@ -1,1 +1,21 @@
-#tuple s
+#tuple(元组) 数据类型,不想让用户更改数据，tuple运算比较快
+my_list = [1, 2, 3, 4, 5]
+my_tuple = (1, 2, 3, 4, 5, "wrf")
+print(type(my_list))
+print(type(my_tuple))
+print(len(my_list))
+print(len(my_tuple))
+print(my_list[0])
+print(my_tuple[0]) # 取元组中的值
+# print(dir(my_list))
+# print("^^^^^^^^^^^^^^^^")
+# print(dir(my_tuple))
+
+
+
+
+
+
+
+
+
