b8af919f7fd4ab570e88b808b5c664d48531bf37
nucfive <18734914520@163.com>
2018-11-05 18:08:57 +0800

    基本使用

---
+++ ex5_dictionary.py
@@ -0,0 +1,16 @@
+#dictionary 字典类型 {}
+my_phone = {"iphone" : 1000, "Sumsang" : 900}
+#取出键中的值 用[]
+iphone_price = my_phone["iphone"]
+print(iphone_price)
+#更改键值
+my_phone["iphone"] = 4000
+print(my_phone["iphone"])
+print("Iphone price :" + str(my_phone["iphone"]))
+my_dic = {1 : "wrf1", 2 : "wrf2", 3 : "wrf3"}
+str1 = my_dic[1]
+my_phone.clear()
+print(my_phone)
+
+
+
