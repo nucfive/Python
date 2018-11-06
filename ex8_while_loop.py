be7fc05106ec2fa31640117821b41b9ef212ffe5
nucfive <18734914520@163.com>
2018-11-06 20:08:33 +0800

    基本使用

---
+++ ex8_while_loop.py
@@ -0,0 +1,11 @@
+#while loop while 循环
+# con = 1
+# while con < 10:
+#     print("hello")
+#     con += 1
+
+while True:
+    a = int(input("please input number a:"))
+    b = int(input("please input number b:"))
+    print("a + b = " + str(a + b))
+
