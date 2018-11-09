08a3f2b597fca3c48673e0dc01c001d1aacf9f1b
nucfive <18734914520@163.com>
2018-11-09 18:28:54 +0800

    基本使用

--- ex10_OOB.py
+++ ex10_OOB.py
@@ -1,1 +1,12 @@
 # oob
+class Student():
+    name = "wrf"
+    age = "19"
+    def eat(self):#self
+        print(self.name + " student can eat, and he is " + self.age)
+    def study(self):
+        print("student can study")
+
+wrf = Student()
+wrf.eat()
+wrf.study()
